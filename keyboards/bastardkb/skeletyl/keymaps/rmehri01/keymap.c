19e0ab4feb1917a54f157160f6268396d2837515
Ryan Mehri <ryan.mehri1@gmail.com>
2024-07-04 09:51:05 -0700

    add swapper and gaming mode

--- keyboards/bastardkb/skeletyl/keymaps/rmehri01/keymap.c
+++ keyboards/bastardkb/skeletyl/keymaps/rmehri01/keymap.c
@@ -17,11 +17,21 @@
 
 #include QMK_KEYBOARD_H
 
+#include "swapper.h"
+
 enum layers {
     BASE,
     SYM,
     NAV,
     FUN,
+    GAMING,
+};
+
+enum keycodes {
+    SW_WIN = SAFE_RANGE, // Switch to next window         (cmd-tab)
+
+    GAMING_ON,
+    GAMING_OFF,
 };
 
 // clang-format off
@@ -42,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
                   KC_7,    KC_5,    KC_3,    KC_1,    KC_9,                         KC_8,    KC_0,    KC_2,    KC_4,    KC_6,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-         OSM(MOD_LCTL),OSM(MOD_LALT),OSM(MOD_LGUI),OSM(MOD_LSFT),XXXXXXX,        XXXXXXX, XXXXXXX,   KC_UP,  KC_ENT, XXXXXXX,
+         OSM(MOD_LCTL),OSM(MOD_LALT),OSM(MOD_LGUI),OSM(MOD_LSFT),XXXXXXX,        XXXXXXX,  SW_WIN,   KC_UP,  KC_ENT, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
                XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, KC_LEFT, KC_DOWN,KC_RIGHT, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
@@ -56,9 +66,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
                KC_SLSH, KC_LPRN, KC_RPRN,  KC_EQL, KC_LBRC,                      KC_RBRC,OSM(MOD_RSFT),OSM(MOD_RGUI),OSM(MOD_RALT),OSM(MOD_RCTL),
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-               KC_QUES, KC_LCBR, KC_RCBR, KC_PLUS, KC_TILD,                      KC_BSLS, KC_MINS, KC_PIPE, XXXXXXX, XXXXXXX,
+               KC_QUES, KC_LCBR, KC_RCBR, KC_PLUS, KC_TILD,                      KC_BSLS, XXXXXXX, KC_PIPE, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                           KC_GRV, KC_LCTL, MO(FUN),    _______, _______, _______
+                                           KC_GRV, KC_MINS, MO(FUN),    _______, _______, _______
                                       //`--------------------------'  `--------------------------'
   ),
 
@@ -68,15 +78,48 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
                 KC_F11,   KC_F4,   KC_F5,   KC_F6, XXXXXXX,              KC_PRINT_SCREEN, KC_VOLU, KC_BRMU, XXXXXXX,  KC_DEL,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-                KC_F10,   KC_F1,   KC_F2,   KC_F3, XXXXXXX,                      KC_CAPS, KC_VOLD, KC_BRMD, XXXXXXX, XXXXXXX,
+                KC_F10,   KC_F1,   KC_F2,   KC_F3, XXXXXXX,                      KC_CAPS, KC_VOLD, KC_BRMD, XXXXXXX,GAMING_ON,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           _______, _______, _______,    _______, _______, _______
+                                      //`--------------------------'  `--------------------------'
+  ),
+  [GAMING] = LAYOUT_split_3x5_3(
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+                  KC_Q,    KC_W,    KC_E,    KC_R,    KC_Y,                         KC_Y,    KC_U,    KC_I,    KC_O, KC_BSPC,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+                  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_QUOT,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+                KC_GRV,  KC_SPC,  KC_TAB,LALT(KC_R),  KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT,GAMING_OFF,
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                             KC_1,    KC_2,    KC_3,  LGUI(KC_TAB),KC_SPC,KC_RSFT
                                       //`--------------------------'  `--------------------------'
   ),
 };
 // clang-format on
 
-const uint16_t PROGMEM jk_combo[]   = {KC_J, KC_K, COMBO_END};
+const uint16_t PROGMEM ht_combo[]   = {KC_H, KC_T, COMBO_END};
 combo_t                key_combos[] = {
-    COMBO(jk_combo, KC_ESC),
+    COMBO(ht_combo, KC_ESC),
 };
+
+bool sw_win_active  = false;
+bool sw_lang_active = false;
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    update_swapper(&sw_win_active, KC_LGUI, KC_TAB, SW_WIN, keycode, record);
+
+    switch (keycode) {
+        case GAMING_ON:
+            if (record->event.pressed) {
+                layer_on(GAMING);
+            }
+            return false;
+        case GAMING_OFF:
+            if (record->event.pressed) {
+                layer_off(GAMING);
+            }
+            return false;
+    }
+
+    return true;
+}
--- keyboards/bastardkb/skeletyl/rules.mk
+++ keyboards/bastardkb/skeletyl/rules.mk
@@ -5,6 +5,9 @@ AUDIO_SUPPORTED = no        # Audio is not supported
 RGB_MATRIX_SUPPORTED = no  # RGB matrix is supported and enabled by default
 RGBLIGHT_SUPPORTED = no    # RGB underglow is supported, but not enabled by default
 
+COMBO_ENABLE = yes
 SPLIT_KEYBOARD = yes
 
 SERIAL_DRIVER = vendor
+
+SRC += keymaps/rmehri01/swapper.c
