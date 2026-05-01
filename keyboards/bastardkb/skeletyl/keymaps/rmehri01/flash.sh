6540e63efe0bec4b2c69f8741f9b185a4cf32849
Ryan Mehri <rmehri01@pm.me>
2026-05-01 18:23:21 -0400

    adjust flash script

--- keyboards/bastardkb/skeletyl/keymaps/rmehri01/flash.sh
+++ keyboards/bastardkb/skeletyl/keymaps/rmehri01/flash.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+make bastardkb/skeletyl/splinktegrated_rev1:rmehri01
 while true
 do
-  mount /dev/sda1 /mnt && cp bastardkb_skeletyl_rmehri01.uf2 /mnt && umount /mnt && echo 'done!' && exit
+  mount /dev/sda1 /mnt && cp bastardkb_skeletyl_splinktegrated_rev1_rmehri01.uf2 /mnt && umount /mnt && echo 'done!' && exit
   sleep 1
 done
